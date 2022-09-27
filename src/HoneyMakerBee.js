2d5e1d59ac2526fd7fdc0e1476358cfbae3b526b
HarryMeng <harry.mld.2010@gmail.com>
2022-09-26 19:52:35 -0700

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+};
+// Inherited the Bee's prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+//Set the constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// a makeHoney method that adds 1 to that honeyBee's honeyPot
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
 };
 
+// a giveHoney method that subtracts 1 from that honeyBee's honeyPot
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
