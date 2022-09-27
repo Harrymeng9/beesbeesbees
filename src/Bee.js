3056afcf1442500787fe2baf2a158f04f307f0f4
HarryMeng <harry.mld.2010@gmail.com>
2022-09-26 19:44:13 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
-};
+  //calls the Grub contructor but passes 'this'
+  Grub.call(this);
 
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+};
+// Inherited the Grub's prototype
+Bee.prototype = Object.create(Grub.prototype);
+// .prototype.constructor property that points back to the constructor itself
+Bee.prototype.constructor = Bee;
